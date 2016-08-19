ebafb80ccf053328a6fb0fbc51c238c1a5f4d2f1
vanthanhkok <ntthoan.rhm35@student.ctump.edu.vn>
2016-08-19 10:31:02 +0700

    Update 2 action get autho and get key

--- function.php
+++ function.php
@@ -6,4 +6,10 @@ class auto extends Mage_Controller {
 	public function __construct(){
 		echo __METHOD__;//Echo method
 	}
+	publuc function getAuth(){
+		//Method get Auth from service
+	}
+	public function getKey(){
+		//Process key form get author
+	}
 }
